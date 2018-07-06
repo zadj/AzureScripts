372c5d26adcd5ba65a6194a94edc7fe0a1f7be5f
zadj <zadj@users.noreply.github.com>
2018-07-06 16:36:35 +0100

    Create test.ps1
    test file to see why it is timing out

---
+++ test.ps1
@@ -0,0 +1,55 @@
+
+# create user
+
+$password = ConvertTo-SecureString "password"  -AsPlainText -Force
+
+
+New-LocalUser `
+   -AccountNeverExpires `
+   -Name "svc1" `
+   -Password $password `
+   -PasswordNeverExpire
+
+
+
+
+# Declare Functions
+
+Function GrantServiceLogon {
+[cmdletbinding()]
+Param (
+  [string] $username = ("{0}\{1}" -f $env:USERDOMAIN, $env:USERNAME)
+   ) 
+# End of Parameters
+Process {
+  $tempPath = [System.IO.Path]::GetTempPath()
+  $import = Join-Path -Path $tempPath -ChildPath "import.inf"
+  if(Test-Path $import) { Remove-Item -Path $import -Force }
+  $export = Join-Path -Path $tempPath -ChildPath "export.inf"
+  if(Test-Path $export) { Remove-Item -Path $export -Force }
+  $secedt = Join-Path -Path $tempPath -ChildPath "secedt.sdb"
+  if(Test-Path $secedt) { Remove-Item -Path $secedt -Force }
+  try {
+    Write-Host ("Granting SeServiceLogonRight to user account: {0}." -f $username)
+    $sid = ((New-Object System.Security.Principal.NTAccount($username)).Translate([System.Security.Principal.SecurityIdentifier])).Value
+
+    Write-Host ("The Sid is {0}" -f $sid)
+    secedit /export /cfg $export
+    $sids = (Select-String $export -Pattern "SeServiceLogonRight").Line
+    Write-Host ("The Sids are {0}" -f $sids)
+    foreach ($line in @("[Unicode]", "Unicode=yes", "[System Access]", "[Event Audit]", "[Registry Values]", "[Version]", "signature=`"`$CHICAGO$`"", "Revision=1", "[Profile Description]", "Description=GrantLogOnAsAService security template", "[Privilege Rights]", "SeServiceLogonRight = *$sids,*$sid")){
+      Add-Content $import $line
+        Write-Host ("Add line {1} to {0}" -f $import, $line)
+    }
+    secedit /import /db $secedt /cfg $import
+    secedit /configure /db $secedt
+    gpupdate /force
+    Remove-Item -Path $import -Force
+    Remove-Item -Path $export -Force
+    Remove-Item -Path $secedt -Force
+  } catch {
+    Write-Host ("Failed to grant SeServiceLogonRight to user account: {0}." -f $username)
+    $error[0]
+  }
+}
+}
